b4f807790c3d2156b6b801b2ae0a9927da5cf4a3
cabosh <callum.bosher@gmail.com>
2020-05-11 15:21:25 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
-    #write your code below this line
+
+    greeting = input("Greetings! How are you doing?")
+    print(greeting)
+    response = input("Oh, how interesting. Tell me more!")
+    print(response)
+    print("Thanks for sharing!")
 
 if __name__ == '__main__':
     main()
